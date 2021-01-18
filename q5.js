59b5d795c5a05a203ffe7d4e67d3fb91556bb85f
DGorgula <gil@dankner.org>
2021-01-18 18:26:49 +0200

    q5 added

---
+++ q5.js
@@ -0,0 +1,10 @@
+// q5 - for loop to check the numbers 0-15 if odd or even
+
+for (let i = 0; i < 16; i++) {
+ if(i%2===0){
+     alert(i+" is even");
+    }
+    else{
+    alert(i+" is odd");
+    }
+}
